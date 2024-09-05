1730d7037fc7258f96d40bf3432f39b414fddf1c
vzoid <viktor@novak.is>
2024-09-05 17:59:38 +0200

    upload

--- repairwinget.ps1
+++ repairwinget.ps1
@@ -1,3 +1,4 @@
+# setup winget
 $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
 $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
         Select-Object -ExpandProperty "assets" |
@@ -7,3 +8,14 @@ $URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
 Invoke-WebRequest -Uri $URL -OutFile "Setup.msix" -UseBasicParsing
 Add-AppxPackage -Path "Setup.msix"
 Remove-Item "Setup.msix"
+
+# install programs
+winget install Git.Git
+winget install Python.Python.3.11
+winget install Docker.DockerDesktop
+winget install OpenJS.NodeJS
+winget install JetBrains.DataGrip
+winget install JetBrains.WebStorm
+winget install Microsoft.VisualStudio.2022.Community.Preview
+winget install Atlassian.Sourcetree
+winget install Posit.RStudio
